f43a55fd611dfe9a68232b3c5369f350032f61aa
Alan Baldwin <baldwingaming1979@gmail.com>
2022-07-11 12:51:03 +0100

    Added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,17 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+             child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+        ],
+      ),
+    );
   }
 }
