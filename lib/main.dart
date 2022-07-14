d9f589cf38338e6f6b32345babee60e6acd836b9
Alan Baldwin <baldwingaming1979@gmail.com>
2022-07-14 13:47:34 +0100

    Added state

--- lib/main.dart
+++ lib/main.dart
@@ -26,14 +26,11 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
 
-  int leftDiceNumber = 0;
-  int rightDiceNumber = 0;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-
-    leftDiceNumber = 2;
-    rightDiceNumber = 5;
 
     return Center(
       child: Row(
@@ -41,7 +38,10 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Left button was pressed');
+                setState((){
+                  leftDiceNumber = 5;
+                  print('Left clicked');
+                });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -49,7 +49,10 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Right button was pressed');
+                setState((){
+                  rightDiceNumber = 2;
+                  print('Right clicked');
+                });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),),
           ),
