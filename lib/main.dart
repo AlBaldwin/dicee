78311dbd7dbf7fbb7d93a03dbd299188be6d44c1
Alan Baldwin <baldwingaming1979@gmail.com>
2022-07-14 13:34:32 +0100

    Moved to a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,22 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 0;
+  int rightDiceNumber = 0;
+
+  @override
   Widget build(BuildContext context) {
+
+    leftDiceNumber = 2;
+    rightDiceNumber = 5;
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-             child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: () {
+                print('Left button was pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
           ),
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: () {
+                print('Right button was pressed');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),),
           ),
         ],
       ),
     );
   }
 }
+
